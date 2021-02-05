e26357ba345bb6e2d035ea1ac5e6840c50dbdae9
xsw2-2wsx <jan1undrych@gmail.com>
2021-02-05 13:37:07 +0100

    Upgrade gradle to version 6.7.1 and add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     java
     kotlin("jvm") version "1.4.21"
+    kotlin("kapt") version "1.4.21"
 }
 
 group = "com.xsw22wsx"
@@ -12,5 +13,39 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    testCompile("junit", "junit", "4.12")
+
+    implementation("org.jsoup:jsoup:1.13.1")
+
+    // Dagger
+    val daggerVersion = "2.29.1"
+    api("com.google.dagger:dagger:$daggerVersion")
+    kapt("com.google.dagger:dagger-compiler:$daggerVersion")
+
+    // Logging
+    val log4jVersion = "2.14.0"
+    implementation("org.apache.logging.log4j:log4j-api:$log4jVersion")
+    runtimeOnly("org.apache.logging.log4j:log4j-core:$log4jVersion")
+
+    // Testing
+    val junitVersion = "5.7.0"
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+    kaptTest("com.google.dagger:dagger-compiler:$daggerVersion")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(8))
+    }
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
